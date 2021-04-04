538342a281701ce7eeb773937fb1ccb5e85c5cad
Ivan Chinea <chitenavi@gmail.com>
2021-04-04 18:35:41 +0100

    Fix: user-router

--- src/resources/user/user.router.js
+++ src/resources/user/user.router.js
@@ -5,6 +5,9 @@ const router = Router()
 
 router.route('/').get(controllers.me).put(controllers.updateMe)
 
+router.route('/find-all').get(controllers.findAll)
+router.route('/add-list').post(controllers.addList)
+
 router.route('/:id').get(controllers.getOne).delete(controllers.removeOne)
 
 export default router
