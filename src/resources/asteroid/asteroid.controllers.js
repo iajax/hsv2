4beff803d2d389345247ec42794b3c29c5ea8b43
Richard C <inf.darze@gmail.com>
2021-04-06 00:03:13 -0500

    refactor: controllers & routes

--- src/resources/asteroid/asteroid.controllers.js
+++ src/resources/asteroid/asteroid.controllers.js
@@ -1,33 +1,4 @@
 import { crudControllers } from '../../utils'
 import { Asteroid } from './asteroid.model'
 
-export const findAll = async (req, res) => {
-  try {
-    const asteroids = await Asteroid.find().lean().exec()
-
-    return res.status(200).send({ data: { asteroids } })
-  } catch (err) {
-    return res.status(500).end()
-  }
-}
-
-export const addList = async (req, res) => {
-  try {
-    const { asteroids } = req.body
-    let data = {}
-
-    if (asteroids?.length) {
-      data.users = await Asteroid.create(asteroids)
-    }
-
-    return res.status(200).send({ data })
-  } catch (err) {
-    return res.status(500).end()
-  }
-}
-
-export default {
-  ...crudControllers(Asteroid),
-  findAll,
-  addList,
-}
+export default crudControllers(Asteroid)
